33ad52a2db937d3c2358c1b942a228bfda078d38
Mohamed-Moawad <mohamedmoawad531@icloud.com>
2022-11-02 18:05:25 +0200

    my project

---
+++ min.js
@@ -0,0 +1,34 @@
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+var num5 = 50;
+var num6 = 10;
+var num7 = 70;
+var num8 = 80;
+var num9 = 90;
+var num10 = 100;
+
+console.log((num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10)/10);
+
+let img = document.getElementById('kkk');
+function earphones(ear){
+    img.src = ear;
+
+}
+
+function ColorToggle() {
+    var BODY = document.body;
+    BODY.classList.toggle("light_mode");
+    
+}
+
+function opennav() {
+    document.getElementById('links').style.width = "500px";
+}
+
+function clossenav() {
+    document.getElementById('links').style.width = "0";
+}
+
+
